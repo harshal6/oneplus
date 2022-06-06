2472697f01db3c24e742687700fb3bc87f2ba33d
Sagar <sagarbharvadiya@gmail.com>
2022-06-06 16:39:59 +0530

    added SEO tags

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,17 @@ const App = () => {
         <title>One-Plus</title>
         <meta name="info" content="this is one-plus website"></meta>
         <meta name="keywords" content="one-plus , service"></meta>
+        <meta name="title" content=": Unique Marketing Consultancy for Asian brands across the world "></meta>
+        <meta name="description" content=":We are a team of marke+ng experts with a vision to help your organisa+on with proven 
+        marke+ng methodologies that interpret and adapt to the changing world and deliver effec+ve results. "></meta>
+        <meta name="Seo tags" content=":Digital marke,ng, UK marke,ng, Chinese marke,ng, content marke,ng, influencer marke,ng, Indian 
+        company, Japan company, Marke,ng, Market analysis, content, SEO, SEM, SMM, influencer, London, 
+        agency, video, branding, brand, campaign, strategy, research, asian, SME, startup, business, design, brand 
+        book, adver,sing, crea,ve, innova,on, visual iden,ty, market entry, brand architecture, engagement, 
+        consumer journey, customer mapping, communica,on, western market, case study, personalised 
+        marke,ng, brand strategy, brand health, culture development, customer engagement, exper,se, Reputa,on 
+        management, ORM, growth marke,ng, effec,ve marke,ng solu,on, result oriented, KPI, ROI, data driven, 
+        insighJul"></meta>
       </Helmet>
       {/* <Brand data={landingPageData.Brand} /> */}
       <Vision data={landingPageData.Vision} />
