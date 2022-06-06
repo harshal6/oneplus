a5586b9fe2ce7a73452caac979ec22739649eee1
Sagar <sagarbharvadiya@gmail.com>
2022-06-06 18:55:06 +0530

    remove bulleted points in OUR SERVICES and fix and bannar text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,27 +6,12 @@ export const Header = (props) => {
           <div className="container">
             {/* <div className="row"> */}
               <div className="intro-text">
-                <p>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </p>
+                <p className="intro-first-p">{props.data ? props.data.title : "Loading"}</p>
+                <p>{props.data ? props.data.title1 : "Loading"}</p>
                 <h1>{props.data ? props.data.paragraph : "Loading"}</h1>
-                {/* <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "} */}
+              
               </div>
-              {/* <div className="col-md-8  col-md-offset-2 intro-img">
-                {" "}
-                <img
-                  src="img/digital-marketing-company-usa.png"
-                  className="img-responsive"
-                  alt=""
-                />{" "}
-              </div> */}
-            {/* </div> */}
+             
           </div>
         </div>
       </div>
